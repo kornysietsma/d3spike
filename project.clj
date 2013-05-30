d02e95067cfc88204b412952207ffa1f395f66b1
Kornelis Sietsma <korny@guldur.fritz.box>
2013-05-30 21:58:36 +1000

    added bubble chart sample

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
                  [compojure "1.1.5"]
                  [ring "1.1.8"]
                  [ring/ring-json "0.2.0"]
-                 [cheshire "5.2.0"]]
+                 [cheshire "5.2.0"]
+                 [org.clojure/data.csv "0.1.2"]]
   :plugins [[lein-ring "0.8.2"]]
   :ring {
          :handler d3spike.core/app
--- src/d3spike/core.clj
+++ src/d3spike/core.clj
@@ -3,15 +3,31 @@
             [compojure.handler :as handler]
             [compojure.route :as route]
             [ring.util.response :as resp]
+            [cheshire.core :as cheshire]
+            [clojure.data.csv :as csv]
+            [clojure.java.io :as io]
             [ring.middleware.json :as json]))
+
+(defn csv-to-maps [data]
+  (let [headers (first data)
+        rows (rest data)]
+    (map #(zipmap headers %) rows)))
+
+(defn bubbledata []
+  (with-open [in-file (io/reader "static/data/scatter-data.csv")]
+    (doall (csv-to-maps
+            (csv/read-csv in-file)))))
 
 (defroutes app-routes
   (GET "/" [] (resp/file-response "index.html" {:root "static"}))
+  (GET "/bubble.json" [] (bubbledata))
   (route/files "/" {:root "static"})
   (route/not-found "<h1>Page not found</h1>"))
 
 (def app
   (-> (handler/api app-routes)
-    (json/wrap-json-body {:keywords? true})))
+      (json/wrap-json-body {:keywords? true})
+      (json/wrap-json-params)
+      (json/wrap-json-response)))
 
 
